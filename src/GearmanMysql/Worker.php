6005dc0d2c5fbec08a37be4f40fee7a39622fa72
Ross Riley <riley.ross@gmail.com>
2012-09-27 19:51:02 +0100

    load in config

--- src/GearmanMysql/Worker.php
+++ src/GearmanMysql/Worker.php
@@ -21,7 +21,7 @@ class Worker {
   
   public function run() {
     error_log( "Attempting to run job from GearmanMysql");
-    
+    Config::load("db.ini");
     if($db = Config::get('db')) Model::load_adapter($db);
 
     $jq = new Queue("runnable");
